b404af258829f5aafb3525e2bee4af8248615dbf
Andresm98 <andresm17u7@hotmail.com>
2026-01-27 11:53:57 -0500

    Challenge microservices init.

---
+++ customer-service/src/test/java/com/anax/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anax.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
